1d9698cabc747e7467b38074db9e76ce25aed21b
unix-pandora <wvwoov@163.com>
2023-02-08 18:25:11 +0800

    small changes

--- kits/ReadFileContent.py
+++ kits/ReadFileContent.py
@@ -6,13 +6,3 @@ def open_file(filename):
     contents = file.readlines()  # 读取所有行
     file.close()  # 关闭文件
     return contents  # 返回读取到的内容
-
-
-# 定义要读取的文件路径
-# file_path = './ask-question.txt'
-
-# 调用函数
-# content = open_file(file_path)
-
-# 输出读取的内容
-# print(content)
--- kits/Robot.py
+++ kits/Robot.py
@@ -36,6 +36,3 @@ def getResp(puzzle):
     print(respondMsg)
 
     return respondMsg
-
-
-# getResp(puzzle)
